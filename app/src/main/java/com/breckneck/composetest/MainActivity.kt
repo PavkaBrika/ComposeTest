4cb795d7a8772c5d305dcfbf11304002eaac9b71
PavkaBrika <pavlikbrichkin@mail.ru>
2022-09-08 19:24:16 +0500

    created animation on image click

--- app/src/main/java/com/breckneck/composetest/MainActivity.kt
+++ app/src/main/java/com/breckneck/composetest/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.breckneck.composetest
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.breckneck.composetest.ui.theme.ComposeTestTheme
+import java.net.InterfaceAddress
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,12 +35,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Toolbar()
+                    Interface()
                 }
             }
         }
     }
 }
+
+@Preview
+@Composable
+fun Interface() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Toolbar()
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            CardCompose()
+        }
+    }
+}
+
+@Composable
+fun CardCompose() {
+    Card() {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+        Column(Modifier.clickable {
+            expanded = !expanded
+        }) {
+            Image(painter = painterResource(id = R.drawable.image), contentDescription = "ds")
+            AnimatedVisibility(visible = expanded) {
+                Text(
+                    text = "Its me",
+                    style = MaterialTheme.typography.h2
+                )
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 fun Toolbar() {
@@ -48,7 +90,7 @@ fun Toolbar() {
                 textAlign = TextAlign.Start,
                 modifier = Modifier
                     .weight(1f)
-                    .padding(16.dp, 16.dp, 16.dp, 1.dp)
+                    .padding(8.dp, 8.dp, 8.dp, 8.dp)
             )
             Text(
                 text = "Settings",
@@ -56,11 +98,13 @@ fun Toolbar() {
                 textAlign = TextAlign.End,
                 modifier = Modifier
                     .weight(1f)
-                    .padding(16.dp, 16.dp, 16.dp, 1.dp)
+                    .padding(8.dp, 8.dp, 8.dp, 8.dp)
             )
         }
         Row(
-            modifier = Modifier.fillMaxWidth().height(1.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp),
         ) {
             Text(
                 text = "",
